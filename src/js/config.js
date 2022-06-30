63ff5ff7eef391a49693cfa63773aeaf73fed742
GretaGrimale <greta.gofmanaite@gmail.com>
2022-06-30 14:33:33 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e582b46e-7b21-4ee5-8279-e79268ef6ef0';
+export const MODAL_CLOSE_SEC = 2.5;
